701e53eb6c04657c900d44b61354d0802280e4b4
Igor Nery <igornery.o@gmail.com>
2018-06-13 15:38:43 -0300

    EigenLib linkado
    Comecando!

---
+++ cmake-build-debug/CMakeFiles/FEMproject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FEMproject.dir/main.cpp.obj"
+  "FEMproject.pdb"
+  "FEMproject.exe"
+  "FEMproject.exe.manifest"
+  "libFEMproject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FEMproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
