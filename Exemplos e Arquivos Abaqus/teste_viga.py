3ad825f952a22e58248ed857f7228792716a197a
Lucas Omena <lucasomena9@gmail.com>
2018-06-14 09:29:33 -0300

    Exemplo Abaqus

---
+++ Exemplos e Arquivos Abaqus/teste_viga.py
@@ -0,0 +1,175 @@
+# -*- coding: mbcs -*-
+# Do not delete the following import lines
+from abaqus import *
+from abaqusConstants import *
+import __main__
+
+def viga_plana_01():
+    import section
+    import regionToolset
+    import displayGroupMdbToolset as dgm
+    import part
+    import material
+    import assembly
+    import step
+    import interaction
+    import load
+    import mesh
+    import optimization
+    import job
+    import sketch
+    import visualization
+    import xyPlot
+    import displayGroupOdbToolset as dgo
+    import connectorBehavior
+    s1 = mdb.models['Model-1'].ConstrainedSketch(name='__profile__', sheetSize=1.0)
+    g, v, d, c = s1.geometry, s1.vertices, s1.dimensions, s1.constraints
+    s1.setPrimaryObject(option=STANDALONE)
+    s1.rectangle(point1=(-0.24, 0.065), point2=(0.085, 0.0))
+    s1.ObliqueDimension(vertex1=v[1], vertex2=v[2], textPoint=(1.0, 0.0), 
+        value=1.0)
+    session.viewports['Viewport: 1'].view.setValues(nearPlane=0.735605, 
+        farPlane=1.15001, width=2.05147, height=1.15711, cameraPosition=(
+        0.543683, 0.122554, 0.942809), cameraTarget=(0.543683, 0.122554, 0))
+    s1.VerticalDimension(vertex1=v[3], vertex2=v[2], textPoint=(0.137519299983978, 
+        0.00943662226200104), value=0.2)
+    session.viewports['Viewport: 1'].view.setValues(nearPlane=0.69573, 
+        farPlane=1.18989, width=2.44627, height=1.37979, cameraPosition=(
+        0.688353, 0.0891379, 0.942809), cameraTarget=(0.688353, 0.0891379, 0))
+    p = mdb.models['Model-1'].Part(name='Part-1', dimensionality=TWO_D_PLANAR, 
+        type=DEFORMABLE_BODY)
+    p = mdb.models['Model-1'].parts['Part-1']
+    p.BaseShell(sketch=s1)
+    s1.unsetPrimaryObject()
+    p = mdb.models['Model-1'].parts['Part-1']
+    session.viewports['Viewport: 1'].setValues(displayedObject=p)
+    del mdb.models['Model-1'].sketches['__profile__']
+    session.viewports['Viewport: 1'].partDisplay.setValues(sectionAssignments=ON, 
+        engineeringFeatures=ON)
+    session.viewports['Viewport: 1'].partDisplay.geometryOptions.setValues(
+        referenceRepresentation=OFF)
+    mdb.models['Model-1'].Material(name='Steel')
+    mdb.models['Model-1'].materials['Steel'].Elastic(table=((200000000.0, 0.3), ))
+    mdb.models['Model-1'].HomogeneousSolidSection(name='steel', material='Steel', 
+        thickness=0.2)
+    p = mdb.models['Model-1'].parts['Part-1']
+    f = p.faces
+    faces = f.getSequenceFromMask(mask=('[#1 ]', ), )
+    region = p.Set(faces=faces, name='Set-1')
+    p = mdb.models['Model-1'].parts['Part-1']
+    p.SectionAssignment(region=region, sectionName='steel', offset=0.0, 
+        offsetType=MIDDLE_SURFACE, offsetField='', 
+        thicknessAssignment=FROM_SECTION)
+    a = mdb.models['Model-1'].rootAssembly
+    session.viewports['Viewport: 1'].setValues(displayedObject=a)
+    session.viewports['Viewport: 1'].assemblyDisplay.setValues(
+        adaptiveMeshConstraints=ON, optimizationTasks=OFF, 
+        geometricRestrictions=OFF, stopConditions=OFF)
+    mdb.models['Model-1'].StaticLinearPerturbationStep(name='Load', 
+        previous='Initial')
+    session.viewports['Viewport: 1'].assemblyDisplay.setValues(step='Load')
+    session.viewports['Viewport: 1'].assemblyDisplay.setValues(
+        adaptiveMeshConstraints=OFF)
+    a = mdb.models['Model-1'].rootAssembly
+    a.DatumCsysByDefault(CARTESIAN)
+    p = mdb.models['Model-1'].parts['Part-1']
+    a.Instance(name='Part-1-1', part=p, dependent=ON)
+    session.viewports['Viewport: 1'].assemblyDisplay.setValues(loads=ON, bcs=ON, 
+        predefinedFields=ON, connectors=ON)
+    session.viewports['Viewport: 1'].assemblyDisplay.setValues(step='Initial')
+    a = mdb.models['Model-1'].rootAssembly
+    e1 = a.instances['Part-1-1'].edges
+    edges1 = e1.getSequenceFromMask(mask=('[#2 ]', ), )
+    region = a.Set(edges=edges1, name='Set-1')
+    mdb.models['Model-1'].EncastreBC(name='engaste', createStepName='Initial', 
+        region=region, localCsys=None)
+    session.viewports['Viewport: 1'].assemblyDisplay.setValues(step='Load')
+    a = mdb.models['Model-1'].rootAssembly
+    v1 = a.instances['Part-1-1'].vertices
+    verts1 = v1.getSequenceFromMask(mask=('[#1 ]', ), )
+    region = a.Set(vertices=verts1, name='Set-2')
+    mdb.models['Model-1'].ConcentratedForce(name='Point_load', 
+        createStepName='Load', region=region, cf2=-4.0, 
+        distributionType=UNIFORM, field='', localCsys=None)
+    session.viewports['Viewport: 1'].assemblyDisplay.setValues(mesh=ON, loads=OFF, 
+        bcs=OFF, predefinedFields=OFF, connectors=OFF)
+    session.viewports['Viewport: 1'].assemblyDisplay.meshOptions.setValues(
+        meshTechnique=ON)
+    p = mdb.models['Model-1'].parts['Part-1']
+    session.viewports['Viewport: 1'].setValues(displayedObject=p)
+    session.viewports['Viewport: 1'].partDisplay.setValues(sectionAssignments=OFF, 
+        engineeringFeatures=OFF, mesh=ON)
+    session.viewports['Viewport: 1'].partDisplay.meshOptions.setValues(
+        meshTechnique=ON)
+    p = mdb.models['Model-1'].parts['Part-1']
+    f = p.faces
+    pickedRegions = f.getSequenceFromMask(mask=('[#1 ]', ), )
+    p.setMeshControls(regions=pickedRegions, elemShape=QUAD, technique=STRUCTURED)
+    p = mdb.models['Model-1'].parts['Part-1']
+    e = p.edges
+    pickedEdges = e.getSequenceFromMask(mask=('[#2 ]', ), )
+    p.seedEdgeByNumber(edges=pickedEdges, number=2, constraint=FINER)
+    p = mdb.models['Model-1'].parts['Part-1']
+    e = p.edges
+    pickedEdges = e.getSequenceFromMask(mask=('[#8 ]', ), )
+    p.seedEdgeByNumber(edges=pickedEdges, number=2, constraint=FINER)
+    p = mdb.models['Model-1'].parts['Part-1']
+    e = p.edges
+    pickedEdges = e.getSequenceFromMask(mask=('[#1 ]', ), )
+    p.seedEdgeByNumber(edges=pickedEdges, number=10, constraint=FINER)
+    p = mdb.models['Model-1'].parts['Part-1']
+    e = p.edges
+    pickedEdges = e.getSequenceFromMask(mask=('[#4 ]', ), )
+    p.seedEdgeByNumber(edges=pickedEdges, number=10, constraint=FINER)
+    elemType1 = mesh.ElemType(elemCode=CPS4R, elemLibrary=STANDARD, 
+        secondOrderAccuracy=OFF, hourglassControl=DEFAULT, 
+        distortionControl=DEFAULT)
+    elemType2 = mesh.ElemType(elemCode=CPS3, elemLibrary=STANDARD)
+    p = mdb.models['Model-1'].parts['Part-1']
+    f = p.faces
+    faces = f.getSequenceFromMask(mask=('[#1 ]', ), )
+    pickedRegions =(faces, )
+    p.setElementType(regions=pickedRegions, elemTypes=(elemType1, elemType2))
+    p = mdb.models['Model-1'].parts['Part-1']
+    p.generateMesh()
+    a = mdb.models['Model-1'].rootAssembly
+    a.regenerate()
+    session.viewports['Viewport: 1'].setValues(displayedObject=a)
+    session.viewports['Viewport: 1'].assemblyDisplay.setValues(mesh=OFF)
+    session.viewports['Viewport: 1'].assemblyDisplay.meshOptions.setValues(
+        meshTechnique=OFF)
+    mdb.Job(name='Job-1', model='Model-1', description='', type=ANALYSIS, 
+        atTime=None, waitMinutes=0, waitHours=0, queue=None, memory=90, 
+        memoryUnits=PERCENTAGE, getMemoryFromAnalysis=True, 
+        explicitPrecision=SINGLE, nodalOutputPrecision=SINGLE, echoPrint=OFF, 
+        modelPrint=OFF, contactPrint=OFF, historyPrint=OFF, userSubroutine='', 
+        scratch='', resultsFormat=ODB)
+    mdb.jobs['Job-1'].writeInput(consistencyChecking=OFF)
+    del mdb.jobs['Job-1']
+    mdb.Job(name='teste_viga', model='Model-1', description='', type=ANALYSIS, 
+        atTime=None, waitMinutes=0, waitHours=0, queue=None, memory=90, 
+        memoryUnits=PERCENTAGE, getMemoryFromAnalysis=True, 
+        explicitPrecision=SINGLE, nodalOutputPrecision=SINGLE, echoPrint=OFF, 
+        modelPrint=OFF, contactPrint=OFF, historyPrint=OFF, userSubroutine='', 
+        scratch='', resultsFormat=ODB)
+    mdb.jobs['teste_viga'].writeInput(consistencyChecking=OFF)
+    mdb.jobs['teste_viga'].submit(consistencyChecking=OFF)
+    session.mdbData.summary()
+    o3 = session.openOdb(
+        name='C:/Users/lucas_omena/Dropbox/Mestrado/MEF/teste_viga.odb')
+    session.viewports['Viewport: 1'].setValues(displayedObject=o3)
+    session.viewports['Viewport: 1'].odbDisplay.display.setValues(plotState=(
+        CONTOURS_ON_DEF, ))
+    session.viewports['Viewport: 1'].odbDisplay.display.setValues(plotState=(
+        UNDEFORMED, ))
+    session.viewports['Viewport: 1'].odbDisplay.display.setValues(plotState=(
+        CONTOURS_ON_DEF, ))
+    session.animationController.setValues(animationType=SCALE_FACTOR, viewports=(
+        'Viewport: 1', ))
+    session.animationController.play(duration=UNLIMITED)
+    session.viewports['Viewport: 1'].odbDisplay.display.setValues(plotState=(
+        UNDEFORMED, ))
+    session.viewports['Viewport: 1'].odbDisplay.display.setValues(plotState=(
+        CONTOURS_ON_DEF, ))
+
+
